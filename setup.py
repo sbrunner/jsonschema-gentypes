8eb3431c94778cef1d267de6e878d2465c8e1550
Stéphane Brunner <stephane.brunner@camptocamp.com>
2021-06-02 17:00:31 +0200

    Doing release 0.9.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import find_packages, setup
 
 site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
-VERSION = os.environ.get("VERSION", "1.0.0")
+VERSION = os.environ.get("VERSION", "0.9.1")
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 config = configparser.ConfigParser()
