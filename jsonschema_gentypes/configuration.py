cb84aa0b0768745713fd358544f6bac1da8accd6
Stéphane Brunner <stephane.brunner@camptocamp.com>
2022-10-10 09:55:08 +0200

    Typo in the schema, regenerate

--- jsonschema_gentypes/configuration.py
+++ jsonschema_gentypes/configuration.py
@@ -27,14 +27,28 @@ class ApiArguments(TypedDict, total=False):
 class Configuration(TypedDict, total=False):
     headers: str
     callbacks: List[List[str]]
+    # The maximum line length
     lineLength: int
+    # required
     generate: List["GenerateItem"]
 
 
 # Generate item
 class GenerateItem(TypedDict, total=False):
+    # The JSON schema file name
+    #
+    # required
     source: str
+    # The generated Python file name
+    #
+    # required
     destination: str
+    # The name of the root element
     root_name: str
+    # WARNING: The required are not correctly taken in account,
+    # See: https://github.com/camptocamp/jsonschema-gentypes/issues/6
     api_arguments: "ApiArguments"
+    # Name mapping
+    #
+    # Used to map the name of an alternate name
     name_mapping: Dict[str, str]
