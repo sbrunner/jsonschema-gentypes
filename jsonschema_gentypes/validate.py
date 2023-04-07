0f3195731b3bf5e2630be74921ccb3e9e4d33e8b
Stéphane Brunner <stephane.brunner@camptocamp.com>
2023-04-07 19:25:58 +0200

    Add validator

--- jsonschema_gentypes/validate.py
+++ jsonschema_gentypes/validate.py
@@ -2,12 +2,18 @@
 Module that offer some useful functions to validate the data against a JSON schema.
 """
 
+import argparse
+import json
 import logging
 import re
+import sys
 from typing import Any, Dict, Iterator, List, Optional, Tuple
 from warnings import warn
 
 import jsonschema
+import requests
+import ruamel.yaml
+import urlparse
 
 import jsonschema_gentypes.jsonschema
 
@@ -157,3 +163,70 @@ class ValidationError(Exception):
         """
         super().__init__(message)
         self.data = data
+
+
+def main() -> None:
+    """
+    Check the JSON ort YAML files against the JSON schema files.
+    """
+    argparser = argparse.ArgumentParser("Check the JSON ort YAML files against the JSON schema files")
+    argparser.add_argument("--schema", help="The JSON schema")
+    argparser.add_argument("--json", action="store_true", help="Parse as JSON")
+    argparser.add_argument("--yaml", action="store_true", help="Parse as YAML")
+    argparser.add_argument("--timeout", default=30, type=int, help="Timeout in seconds")
+    argparser.add_argument("files", nargs="+", help="The files to check")
+    args = argparser.parse_args()
+
+    if args.json and args.yaml:
+        print("You can not specify both --json and --yaml")
+        sys.exit(1)
+
+    re_ = re.compile(r".*schema=(\S+)")
+    yaml = ruamel.yaml.YAML()
+
+    for file in args.files:
+        is_json = args.json
+        is_yaml = args.yaml
+        if not is_json and not is_yaml:
+            is_json = args.file.endswith(".json")
+            is_yaml = args.file.endswith(".yaml") or args.file.endswith(".yml")
+
+        if not is_json and not is_yaml:
+            print(f"Unknown file type: {file}")
+            sys.exit(1)
+
+        schema = args.schema
+
+        if schema is None and is_yaml:
+            with open(file, encoding="utf-8") as data_file:
+                match = re_.match(data_file.readline())
+                if match:
+                    schema = match.group(1)
+
+        data: Dict[str, Any] = {}
+        if is_yaml:
+            with open(file, encoding="utf-8") as data_file:
+                data = yaml.load(data_file)
+        elif is_json:
+            with open(file, encoding="utf-8") as data_file:
+                data = json.load(data_file)
+
+        if args.schema is None:
+            schema = data.get("$schema")
+
+        if schema is None:
+            print(f"Could not find the schema for {file}")
+            sys.exit(1)
+
+        if schema.startswith("http://"):
+            if urlparse.urlparse(schema).scheme != "":
+                with open(schema, encoding="utf-8") as schema_file:
+                    schema_data = json.load(schema_file)
+            else:
+                response = requests.get(schema, timeout=args.timeout)
+                if not response.ok:
+                    print(f"Could not load the schema {schema}")
+                    sys.exit(1)
+
+                schema_data = response.json()
+        validate(file, data, schema_data)
